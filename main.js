decaec166a990ddcc728e5590c1fa98b769e48d8
Jabin Cuevas <jabin753@gmail.com>
2021-04-20 13:05:21 -0500

    binary input field with constraint

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const binaryNumber = document.getElementById('binaryNumber')
 const decimalResult = document.getElementById('decimalResult')
 const convertBtn = document.getElementById('convertBtn')
 
+// Validation constraint
+binaryNumber.addEventListener('keydown', function(e) {
+    if(e.key == 1 || e.key == 0 || e.key == 'Backspace');
+    else e.preventDefault()
+})
+
 convertBtn.addEventListener('click', function(){
     decimalResult.innerText = bin2Dec(binaryNumber.value)
 })
